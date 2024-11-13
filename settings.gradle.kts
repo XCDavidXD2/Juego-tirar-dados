44aec3ab073d406800ef4572a2ec9a1ca9cc98fd
David Correa <157735635+XCDavidXD2@users.noreply.github.com>
2024-11-13 19:15:25 +0100

    Se agrega el juego

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Roll_the_Dice_David_Correa_y_Carlos_Ayuso"
+include(":app")
+ 
